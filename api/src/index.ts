840b08b798671397754dc4d6e9d62051c232f927
Mijat019 <mijat.miletic98@gmail.com>
2020-04-01 01:58:00 +0200

    Implement Clinics model

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,8 +5,6 @@ import cors from "cors";
 import config from "./config";
 
 import db from "./models/database";
-import ClinicAdmins from "./models/Users/ClinicAdmins";
-import Patients from "./models/Users/Patients";
 
 // connect to the database
 (async () => {
@@ -28,25 +26,6 @@ const app: Application = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-/**
- * OVO JE SAMO TEST
- */
-app.get("/", async (req: any, res: any) => {
-    await Patients.create({
-        email: "dasd",
-        password: "dsf",
-        firstName: "asd",
-        lastName: "adsf",
-        jmbg: "sdf",
-        city: "daf",
-        country: "sdaf",
-        address: "sadf",
-        phoneNumber: "asdf"
-    });
-    const resp = await Patients.findAll();
-    res.send(resp);
-});
 
 app.listen(config.port, () =>
     console.log(`Server listening on port ${config.port}`)
---
+++ api/src/models/Clinics.ts
@@ -0,0 +1,34 @@
+import db from "../database";
+import { Model, STRING, INTEGER } from "sequelize";
+
+class Clinics extends Model {
+    public id!: number;
+    public name!: string;
+    public address!: string;
+    public description!: string;
+}
+
+Clinics.init(
+    {
+        id: {
+            type: INTEGER.UNSIGNED,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: STRING,
+            allowNull: false
+        },
+        address: {
+            type: STRING,
+            allowNull: false
+        },
+        description: STRING
+    },
+    {
+        sequelize: db,
+        tableName: "clinics"
+    }
+);
+
+export default Clinics;
