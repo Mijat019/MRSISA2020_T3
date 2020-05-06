4e117161128f145c76f90fe4594aa636fb04f431
mijat.miletic98 <mijat.miletic98@gmail.com>
2020-05-06 23:35:06 +0200

    Implement create drug action

--- client/src/store/modules/drugs.js
+++ client/src/store/modules/drugs.js
@@ -8,6 +8,10 @@ const mutations = {
   setDrugs(state, drugs) {
     state.drugs = drugs;
   },
+
+  addDrug(state, drug) {
+    state.drugs.push(drug);
+  },
 };
 
 const actions = {
@@ -15,6 +19,16 @@ const actions = {
     try {
       const { data: drugs } = await Vue.$axios.get("/drugs");
       commit("setDrugs", drugs);
+    } catch (error) {
+      dispatch("snackbar/showError", error.response.data, { root: true });
+    }
+  },
+
+  async addDrugAction({ commit, dispatch }, drugPayload) {
+    try {
+      const { data: newDrug } = await Vue.$axios.post("/drugs", drugPayload);
+      commit("addDrug", newDrug);
+      dispatch("snackbar/showSuccess", "New drug added.", { root: true });
     } catch (error) {
       dispatch("snackbar/showError", error.response.data, { root: true });
     }
