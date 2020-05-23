0b90a3dd0a54a307bb4071a000178c50c8193b5b
mijat.miletic98 <mijat.miletic98@gmail.com>
2020-05-23 17:01:25 +0200

    Add typesript package to api and remove all console.logs

--- client/src/store/modules/customAppointment/scheduleCustomAppointment.js
+++ client/src/store/modules/customAppointment/scheduleCustomAppointment.js
@@ -1,79 +1,84 @@
 import Vue from "vue";
 
 const state = {
-  appointmentRequests : []
+    appointmentRequests: [],
 };
 
 const mutations = {
-  setAppointmentRequests(state, requests) {
-    state.appointmentRequests = requests;
-  },
-  addAppointmentRequest(state, newReq) {
-    state.appointmentRequests.push(newReq);
-  },
-  removeAppointmentRequest(state, id) {
-    const index = state.appointmentRequests.findIndex((type) => type.id === id);
-    state.appointmentRequests.splice(index, 1);
-  },
+    setAppointmentRequests(state, requests) {
+        state.appointmentRequests = requests;
+    },
+    addAppointmentRequest(state, newReq) {
+        state.appointmentRequests.push(newReq);
+    },
+    removeAppointmentRequest(state, id) {
+        const index = state.appointmentRequests.findIndex(
+            (type) => type.id === id
+        );
+        state.appointmentRequests.splice(index, 1);
+    },
 };
 
 const actions = {
+    async requestAppointmentAction({ commit, dispatch }, typePayload) {
+        try {
+            const { data: newReq } = await Vue.$axios.post(
+                `/appointmentRequests`,
+                typePayload
+            );
+            commit("addAppointmentRequest", newReq);
+            dispatch(
+                "snackbar/showSuccess",
+                "Appointment request successfully submited.",
+                {
+                    root: true,
+                }
+            );
+        } catch (error) {
+            dispatch("snackbar/showError", error.response.data, { root: true });
+        }
+    },
 
-  async requestAppointmentAction({ commit, dispatch }, typePayload) {
-    try {
-      const { data: newReq } = await Vue.$axios.post(
-        `/appointmentRequests`,
-        typePayload
-      );
-      commit("addAppointmentRequest", newReq);
-      dispatch("snackbar/showSuccess", "Appointment request successfully submited.", {
-        root: true,
-      });
-    } catch (error) {
-      console.log(error);
-      dispatch("snackbar/showError", error.response.data, { root: true });
-    }
-  },
+    async getAllForClinicAction({ commit, dispatch }, clinicId) {
+        try {
+            const { data: newReq } = await Vue.$axios.get(
+                `/appointmentRequests/${clinicId}`
+            );
+            commit("setAppointmentRequests", newReq);
+        } catch (error) {
+            dispatch("snackbar/showError", error.response.data, { root: true });
+        }
+    },
 
-  async getAllForClinicAction({ commit, dispatch }, clinicId) {
-    try {
-      const { data: newReq } = await Vue.$axios.get(`/appointmentRequests/${clinicId}`);
-      commit("setAppointmentRequests", newReq);
-    } catch (error) {
-      console.log(error);
-      dispatch("snackbar/showError", error.response.data, { root: true });
-    }
-  },
+    async confirmRequestAction({ commit, dispatch }, reqPayload) {
+        try {
+            await Vue.$axios.patch(`/appointmentRequests/confirm`, reqPayload);
+            commit("removeAppointmentRequest", reqPayload.id);
+            dispatch("snackbar/showSuccess", "Approved", { root: true });
+        } catch (error) {
+            dispatch("snackbar/showError", error.response.data, { root: true });
+        }
+    },
 
-  async confirmRequestAction({ commit, dispatch }, reqPayload) {
-    try {
-      await Vue.$axios.patch(`/appointmentRequests/confirm`, reqPayload);
-      commit("removeAppointmentRequest", reqPayload.id);
-      dispatch("snackbar/showSuccess", "Approved", { root: true });
-    } catch (error) {
-      dispatch("snackbar/showError", error.response.data, { root: true });
-    }
-  },
-
-  async rejectRequestAction({ commit, dispatch }, reqPayload ) {
-    try {
-      await Vue.$axios.patch(`/appointmentRequests/reject`, reqPayload );
-      commit("removeAppointmentRequest", reqPayload.id);
-      dispatch("snackbar/showSuccess", "Rejected", { root: true });
-    } catch (error) {
-      dispatch("snackbar/showError", error.response.data, { root: true });
-    }
-  },
+    async rejectRequestAction({ commit, dispatch }, reqPayload) {
+        try {
+            await Vue.$axios.patch(`/appointmentRequests/reject`, reqPayload);
+            commit("removeAppointmentRequest", reqPayload.id);
+            dispatch("snackbar/showSuccess", "Rejected", { root: true });
+        } catch (error) {
+            dispatch("snackbar/showError", error.response.data, { root: true });
+        }
+    },
 };
 
 const getters = {
-  getAppointmentRequests: (state) => state.appointmentRequests,
+    getAppointmentRequests: (state) => state.appointmentRequests,
 };
 
 export default {
-  namespaced: true,
-  state,
-  mutations,
-  actions,
-  getters,
+    namespaced: true,
+    state,
+    mutations,
+    actions,
+    getters,
 };
--- client/src/store/modules/freeAppointments.js
+++ client/src/store/modules/freeAppointments.js
@@ -1,131 +1,142 @@
 import Vue from "vue";
 
 const state = {
-  appointments: [],
+    appointments: [],
 };
 
 const mutations = {
-  setAppointments(state, appointments) {
-    state.appointments = appointments;
-  },
-  addAppointment(state, newAppointment) {
-    state.appointments.push(newAppointment);
-  },
-  removeAppointment(state, id) {
-    const index = state.appointments.findIndex((type) => type.id === id);
-    state.appointments.splice(index, 1);
-  },
-  updateAppointment(state, newAppointment) {
-    const index = state.appointments.findIndex(
-      (type) => type.id === newAppointment.id
-    );
-    Object.assign(state.appointments[index], newAppointment);
-  },
+    setAppointments(state, appointments) {
+        state.appointments = appointments;
+    },
+    addAppointment(state, newAppointment) {
+        state.appointments.push(newAppointment);
+    },
+    removeAppointment(state, id) {
+        const index = state.appointments.findIndex((type) => type.id === id);
+        state.appointments.splice(index, 1);
+    },
+    updateAppointment(state, newAppointment) {
+        const index = state.appointments.findIndex(
+            (type) => type.id === newAppointment.id
+        );
+        Object.assign(state.appointments[index], newAppointment);
+    },
 };
 
 const actions = {
-  async getFreeAppointmentsAction({ commit, dispatch }, doctorId) {
-    // no selected doctors
-    if (!doctorId) {
-      commit("setAppointments", []);
-      return;
-    }
+    async getFreeAppointmentsAction({ commit, dispatch }, doctorId) {
+        // no selected doctors
+        if (!doctorId) {
+            commit("setAppointments", []);
+            return;
+        }
 
-    try {
-      let { data: appointments } = await Vue.$axios.get(
-        `/freeAppointments/${doctorId}`
-      );
-      commit("setAppointments", appointments);
-    } catch (error) {
-      dispatch("snackbar/showError", error.response.data, { root: true });
-    }
-  },
+        try {
+            let { data: appointments } = await Vue.$axios.get(
+                `/freeAppointments/${doctorId}`
+            );
+            commit("setAppointments", appointments);
+        } catch (error) {
+            dispatch("snackbar/showError", error.response.data, { root: true });
+        }
+    },
 
-  async getFreeAppointmentsByTypeAction({ commit, dispatch }, typeId) {
-    // no selected doctors
-    if (!typeId) {
-      commit("setAppointments", []);
-      return;
-    }
+    async getFreeAppointmentsByTypeAction({ commit, dispatch }, typeId) {
+        // no selected doctors
+        if (!typeId) {
+            commit("setAppointments", []);
+            return;
+        }
 
-    try {
-      let { data: appointments } = await Vue.$axios.get(
-        `/freeAppointments/type/${typeId}`
-      );
-      commit("setAppointments", appointments);
-    } catch (error) {
-      dispatch("snackbar/showError", error.response.data, { root: true });
-    }
-  },
+        try {
+            let { data: appointments } = await Vue.$axios.get(
+                `/freeAppointments/type/${typeId}`
+            );
+            commit("setAppointments", appointments);
+        } catch (error) {
+            dispatch("snackbar/showError", error.response.data, { root: true });
+        }
+    },
 
-  async addFreeAppointmentAction({ commit, dispatch }, appointmentPayload) {
-    try {
-      const { data: newAppointment } = await Vue.$axios.post(
-        "/freeAppointments",
-        appointmentPayload
-      );
+    async addFreeAppointmentAction({ commit, dispatch }, appointmentPayload) {
+        try {
+            const { data: newAppointment } = await Vue.$axios.post(
+                "/freeAppointments",
+                appointmentPayload
+            );
 
-      commit("addAppointment", newAppointment);
-      dispatch("snackbar/showSuccess", "Appointment successfully added", {
-        root: true,
-      });
-    } catch (error) {
-      dispatch("snackbar/showError", error.response.data, { root: true });
-    }
-  },
+            commit("addAppointment", newAppointment);
+            dispatch("snackbar/showSuccess", "Appointment successfully added", {
+                root: true,
+            });
+        } catch (error) {
+            dispatch("snackbar/showError", error.response.data, { root: true });
+        }
+    },
 
-  async updateFreeAppointmentAction({ commit, dispatch }, appointmentUpdate) {
-    try {
-      console.log(appointmentUpdate);
-      const { data: newAppointment } = await Vue.$axios.patch(
-        `/freeAppointments/${appointmentUpdate.id}`,
-        appointmentUpdate
-      );
-      commit("updateAppointment", newAppointment);
-      dispatch("snackbar/showSuccess", "Appointment successfully updated", {
-        root: true,
-      });
-    } catch (error) {
-      dispatch("snackbar/showError", error.response.data, { root: true });
-    }
-  },
+    async updateFreeAppointmentAction({ commit, dispatch }, appointmentUpdate) {
+        try {
+            const { data: newAppointment } = await Vue.$axios.patch(
+                `/freeAppointments/${appointmentUpdate.id}`,
+                appointmentUpdate
+            );
+            commit("updateAppointment", newAppointment);
+            dispatch(
+                "snackbar/showSuccess",
+                "Appointment successfully updated",
+                {
+                    root: true,
+                }
+            );
+        } catch (error) {
+            dispatch("snackbar/showError", error.response.data, { root: true });
+        }
+    },
 
-  async deleteFreeAppointmentAction({ commit, dispatch }, appointmentId) {
-    try {
-      await Vue.$axios.delete(`/freeAppointments/${appointmentId}`);
-      commit("removeAppointment", appointmentId);
-      dispatch("snackbar/showSuccess", "Appointment successfully deleted", {
-        root: true,
-      });
-    } catch (error) {
-      dispatch("snackbar/showError", error.response.data, { root: true });
-    }
-  },
+    async deleteFreeAppointmentAction({ commit, dispatch }, appointmentId) {
+        try {
+            await Vue.$axios.delete(`/freeAppointments/${appointmentId}`);
+            commit("removeAppointment", appointmentId);
+            dispatch(
+                "snackbar/showSuccess",
+                "Appointment successfully deleted",
+                {
+                    root: true,
+                }
+            );
+        } catch (error) {
+            dispatch("snackbar/showError", error.response.data, { root: true });
+        }
+    },
 
-  async makeAppointmentAction({ commit, dispatch }, { appoId, patientId }) {
-    try {
-      await Vue.$axios.post(`/freeAppointments/schedule`, {
-        appoId,
-        patientId,
-      });
-      commit("removeAppointment", appoId);
-      dispatch("snackbar/showSuccess", "Appointment successfully confirmed", {
-        root: true,
-      });
-    } catch (error) {
-      dispatch("snackbar/showError", error.response.data, { root: true });
-    }
-  },
+    async makeAppointmentAction({ commit, dispatch }, { appoId, patientId }) {
+        try {
+            await Vue.$axios.post(`/freeAppointments/schedule`, {
+                appoId,
+                patientId,
+            });
+            commit("removeAppointment", appoId);
+            dispatch(
+                "snackbar/showSuccess",
+                "Appointment successfully confirmed",
+                {
+                    root: true,
+                }
+            );
+        } catch (error) {
+            dispatch("snackbar/showError", error.response.data, { root: true });
+        }
+    },
 };
 
 const getters = {
-  getFreeAppointments: (state) => state.appointments,
+    getFreeAppointments: (state) => state.appointments,
 };
 
 export default {
-  namespaced: true,
-  state,
-  mutations,
-  actions,
-  getters,
+    namespaced: true,
+    state,
+    mutations,
+    actions,
+    getters,
 };
