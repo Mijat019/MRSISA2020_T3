261b02311cead53bc664f7e3132aafcc098a4b7b
mijat.miletic98 <mijat.miletic98@gmail.com>
2020-06-05 23:16:33 +0200

    Fix merge error

--- api/src/models/PriceLists.ts
+++ api/src/models/PriceLists.ts
@@ -1,26 +1,25 @@
-import { Model, INTEGER, DOUBLE, STRING } from "sequelize";
-import db from "./database";
-import Clinics from "./Clinics";
-import AppointmentTypes from "./AppointmentTypes";
+import { Model, INTEGER, DOUBLE, STRING } from 'sequelize';
+import db from './database';
+import Clinics from './Clinics';
+import AppointmentTypes from './AppointmentTypes';
 
 class PriceLists extends Model {
-    public id!: number;
-    public price!: number;
-    public clinicId!: number;
-    public appointmentTypeId!: number;
+  public id!: number;
+  public price!: number;
+  public clinicId!: number;
+  public appointmentTypeId!: number;
 }
 
 PriceLists.init(
-<<<<<<< HEAD
   {
     clinicId: {
-        type: INTEGER.UNSIGNED,
-        allowNull: false,
+      type: INTEGER.UNSIGNED,
+      allowNull: false,
     },
-    
-    appointmentTypeId : {
-        type: INTEGER.UNSIGNED,
-        allowNull: false,
+
+    appointmentTypeId: {
+      type: INTEGER.UNSIGNED,
+      allowNull: false,
     },
 
     id: {
@@ -30,54 +29,27 @@ PriceLists.init(
       allowNull: false,
     },
 
-    price : {
-        type: DOUBLE,
-        allowNull: false,
-=======
-    {
-        clinicId: {
-            type: INTEGER.UNSIGNED,
-            // primaryKey: true,
-            // unique: false,
-            allowNull: false,
-        },
-
-        appointmentTypeId: {
-            type: INTEGER.UNSIGNED,
-            // primaryKey: true,
-            // unique: false,
-            allowNull: false,
-        },
-
-        id: {
-            type: INTEGER.UNSIGNED,
-            primaryKey: true,
-            autoIncrement: true,
-            allowNull: false,
-        },
-
-        price: {
-            type: DOUBLE,
-            allowNull: false,
-        },
+    price: {
+      type: DOUBLE,
+      allowNull: false,
     },
-    {
-        timestamps: false,
-        tableName: "priceLists",
-        indexes: [{ unique: true, fields: ["clinicId", "appointmentTypeId"] }],
-        sequelize: db,
->>>>>>> feature/KP-108
-    }
+  },
+  {
+    timestamps: false,
+    tableName: 'priceLists',
+    indexes: [{ unique: true, fields: ['clinicId', 'appointmentTypeId'] }],
+    sequelize: db,
+  }
 );
 
-PriceLists.belongsTo(Clinics, { as: "clinic", foreignKey: "clinicId" });
+PriceLists.belongsTo(Clinics, { as: 'clinic', foreignKey: 'clinicId' });
 PriceLists.belongsTo(AppointmentTypes, {
-    as: "appointmentType",
-    foreignKey: "appointmentTypeId",
+  as: 'appointmentType',
+  foreignKey: 'appointmentTypeId',
 });
 AppointmentTypes.hasMany(PriceLists, {
-    as: "priceList",
-    foreignKey: "appointmentTypeId",
+  as: 'priceList',
+  foreignKey: 'appointmentTypeId',
 });
 
 export default PriceLists;
