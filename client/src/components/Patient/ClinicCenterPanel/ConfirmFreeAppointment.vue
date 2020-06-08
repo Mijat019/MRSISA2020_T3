3c467ebfc1a65e1c3fe86b22abaf65d4a7fb69c5
Stefan <stefan.kandic@yahoo.com>
2020-06-08 08:41:50 +0200

    fix god awful freeApp component

---
+++ client/src/components/Patient/ClinicCenterPanel/ConfirmFreeAppointment.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container>
+    <!-- Appointment type: {{ dialogAppo.priceList.appointmentType.name }}
+    <br />
+    Doctor: {{ dialogAppo.doctor.user.firstName }}
+    {{ dialogAppo.doctor.user.lastName }}
+    <br />
+    Room: {{ dialogAppo.room.name }}
+    <br />
+    Time: {{ appointmentTime }} -->
+    <v-row class=" headline font-weight-bold px-3 py-3 black--text">
+      Appointment Summary:
+    </v-row>
+    <v-divider></v-divider>
+    <v-row class="subtitle-1">
+      <v-col cols="6">
+        {{ appoType }} <br />
+        {{ doctor }}
+      </v-col>
+
+      <v-col cols="6" class="pr-6">
+        <v-row justify="end">USD {{ price }}</v-row>
+        <v-row justify="end">{{ date }}</v-row>
+        <v-row justify="end">{{ time }} (30 mins)</v-row>
+      </v-col>
+    </v-row>
+
+    <v-row class="mt-10 px-3">
+      <v-text-field
+        readonly
+        value="Email notification will be sent 24 hours before appointment"
+        outlined
+      >
+        <template slot="prepend-inner">
+          <v-icon class="pr-2">mdi-email-open-outline</v-icon>
+        </template>
+      </v-text-field>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: ['appoType', 'doctor', 'room', 'time', 'date', 'price'],
+
+};
+</script>
+
+<style></style>
--- client/src/components/Patient/ClinicCenterPanel/ScheduleAppointment.vue
+++ client/src/components/Patient/ClinicCenterPanel/ScheduleAppointment.vue
@@ -2,102 +2,149 @@
   <v-card>
     <ScheduleAppointmentDialog />
 
-    <v-card-title>
+    <!-- <v-card-title>
       <v-btn @click="openDialog" dark>Custom Appointment</v-btn>
-    </v-card-title>
+    </v-card-title> -->
 
-    <v-card-title>
-      <v-autocomplete
-        v-model="appoType"
-        :items="getAppointmentTypes"
+    <v-card-title class="mx-8 mb-12 mt-5">
+      Free Appointments
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        prepend-icon="mdi-magnify"
+        label="Search"
+        class="searchInput"
+        single-line
+        hide-details
+      ></v-text-field>
+
+      <v-select
+        class="mx-12 selectInput"
         item-text="name"
-        label="Appointment Type"
-        placeholder="Enter appointment type"
+        :items="getAppointmentTypes"
+        v-model="appoType"
+        menu-props="auto"
+        label="Appointment type"
+        hide-details
+        single-line
         return-object
-      ></v-autocomplete>
+      ></v-select>
     </v-card-title>
 
-    <v-card-text v-if="appoType != null">
-      <v-data-table :items="getFreeAppointments" :headers="headers">
+    <v-card-text>
+      <v-data-table
+        :items="getFreeAppointments"
+        :headers="headers"
+        :search="search"
+      >
         <template v-slot:item.actions="{ item }">
-          <v-icon small class="mr-2" @click="showDialog(item)">mdi-check-bold</v-icon>
+          <v-icon small class="mr-2" @click="showDialog(item)"
+            >mdi-check-bold</v-icon
+          >
+        </template>
+        <template #item.doctor="{ item }"
+          >{{ item.doctor.user.firstName }}
+          {{ item.doctor.user.lastName }}</template
+        >
+        <template #item.start="{ item }">
+          {{ format(item.start) }}
+          <!-- {{func(item)}} -->
         </template>
       </v-data-table>
     </v-card-text>
 
-    <v-dialog v-model="dialog" width="500">
+    <v-dialog v-model="dialog" width="600">
       <v-card>
-        Appointment type: {{ dialogAppo.priceList.appointmentType.name }}
-        <br />
-        Doctor: {{ dialogAppo.doctor.user.firstName }} {{ dialogAppo.doctor.user.lastName }}
-        <br />
-        Room: {{ dialogAppo.room.name }}
-        <br />
-        Time: {{ appointmentTime }}
-        <br />
-        <v-spacer />
-        <v-btn color="primary" @click="makeAppointment()">Make appointment</v-btn>
+        <v-card-text>
+          <ConfirmFreeAppointment
+            :appoType="dialogAppo.priceList.appointmentType.name"
+            :doctor="
+              dialogAppo.doctor.user.firstName +
+                ' ' +
+                dialogAppo.doctor.user.lastName
+            "
+            :room="dialogAppo.room.name"
+            :time="appointmentTime"
+            :date="appointmentDate"
+            :price="dialogAppo.priceList.price"
+          />
+        </v-card-text>
+        <v-card-actions class="mx-10">
+          <v-spacer></v-spacer>
+          <v-btn
+            class="blue white--text px-5 text-none"
+            rounded
+            x-large
+            @click="makeAppointment()"
+            >Make appointment</v-btn
+          >
+        </v-card-actions>
       </v-card>
     </v-dialog>
   </v-card>
 </template>
 
 <script>
-import moment from "moment";
-import { mapGetters, mapActions, mapMutations } from "vuex";
-import ScheduleAppointmentDialog from "./ScheduleAppointmentDialog";
+import moment from 'moment';
+import { mapGetters, mapActions, mapMutations } from 'vuex';
+import ScheduleAppointmentDialog from './ScheduleAppointmentDialog';
+import ConfirmFreeAppointment from './ConfirmFreeAppointment';
 
 export default {
-  name: "ScheduleAppointment",
+  name: 'ScheduleAppointment',
   components: {
-    ScheduleAppointmentDialog
+    ScheduleAppointmentDialog,
+    ConfirmFreeAppointment,
   },
   data() {
     return {
       appoKind: null, // custom or predefined
       appoType: null,
+      search: '',
       headers: [
-        {
-          text: "Start",
-          value: "start"
-        },
-        { text: "Room", value: "room.name" },
-        { text: "Doctors first name", value: "doctor.user.firstName" },
-        { text: "Doctors last name", value: "doctor.user.lastName" },
-        { text: "Appointment type", value: "priceList.appointmentType.name" },
-        { text: "Duration", value: "duration" },
-        { text: "Actions", value: "actions", sortable: false }
+        { text: 'Appointment type', value: 'priceList.appointmentType.name' },
+        { text: 'Doctor', value: 'doctor', align: 'center' },
+        { text: 'Room', value: 'room.name' },
+        { text: 'Scheduled Time', value: 'start', align: 'center' },
+        { text: 'Duration', value: 'duration' },
+        { text: 'Actions', value: 'actions', sortable: false },
       ],
       dialog: false,
       dialogAppo: {
         priceList: {
           appointmentType: {
-            name: ""
-          }
+            name: '',
+          },
         },
         doctor: {
           user: {
-            firstName: "",
-            lastName: ""
-          }
+            firstName: '',
+            lastName: '',
+          },
         },
         room: {
-          name: ""
+          name: '',
         },
-        start: ""
-      }
+        start: '',
+      },
     };
   },
 
   methods: {
+    format(item) {
+      if (!item) return '';
+
+      return moment.unix(item).format('YYYY-MM-DD HH:mm');
+    },
     ...mapActions({
-      getAppointmentTypesAction: "appointmentTypes/getAppointmentTypesAction",
+      getAppointmentTypesAction: 'appointmentTypes/getAppointmentTypesAction',
       getFreeAppointmentsByTypeAction:
-        "freeAppointments/getFreeAppointmentsByTypeAction",
-      makeAppointmentAction: "freeAppointments/makeAppointmentAction"
+        'freeAppointments/getFreeAppointmentsByTypeAction',
+      makeAppointmentAction: 'freeAppointments/makeAppointmentAction',
     }),
-    ...mapMutations("scheduleCustomAppointmentDialog", {
-      openDialog: "openDialog"
+    ...mapMutations({
+      openDialog: 'scheduleCustomAppointmentDialog/openDialog',
+      setAppointments: 'freeAppointments/setAppointments',
     }),
     showDialog: function(item) {
       this.dialogAppo = item;
@@ -106,38 +153,41 @@ export default {
     makeAppointment: function() {
       this.makeAppointmentAction({
         appoId: this.dialogAppo.id,
-        patientId: this.getUser.id
+        patientId: this.getUser.id,
       });
       this.dialog = false;
-    }
+    },
   },
 
   async mounted() {
+    await this.setAppointments([]);
     await this.getAppointmentTypesAction();
   },
 
   computed: {
     ...mapGetters({
-      getAppointmentTypes: "appointmentTypes/getAppointmentTypes",
-      getFreeAppointments: "freeAppointments/getFreeAppointments",
-      getUser: "authentication/getUser",
+      getAppointmentTypes: 'appointmentTypes/getAppointmentTypes',
+      getFreeAppointments: 'freeAppointments/getFreeAppointments',
+      getUser: 'authentication/getUser',
     }),
 
     appointmentTime() {
-      const start = moment
-        .unix(this.dialogAppo.start)
-        .format("YYYY-MM-DD HH:mm");
+      const start = moment.unix(this.dialogAppo.start).format('HH:mm');
       return start;
-    }
+    },
+
+    appointmentDate() {
+      const start = moment.unix(this.dialogAppo.start).format('YYYY-MM-DD');
+      return start;
+    },
   },
 
   watch: {
     appoType(value) {
       this.getFreeAppointmentsByTypeAction(value.id);
-    }
-  }
+    },
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
