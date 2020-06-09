53886b222419ab736e6f86111f8c220ea0749df9
mijat.miletic98 <mijat.miletic98@gmail.com>
2020-06-10 01:02:25 +0200

    Update biznis graf

--- client/src/components/ClinicAdmin/ReportTab/BusinessReport.vue
+++ client/src/components/ClinicAdmin/ReportTab/BusinessReport.vue
@@ -41,31 +41,42 @@
     </v-row>
     <v-row>
       <v-col>
-        <v-menu
-          ref="menu"
-          v-model="menu"
-          :close-on-content-click="false"
-          :return-value.sync="dates"
-          transition="scale-transition"
-          offset-y
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-text-field
-              v-model="dates"
-              label="Date interval"
-              prepend-icon="mdi-calendar"
-              readonly
-              v-bind="attrs"
-              v-on="on"
-            ></v-text-field>
-          </template>
-          <v-date-picker v-model="dates" range no-title scrollable>
-            <v-spacer></v-spacer>
-            <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
-            <v-btn text color="primary" @click="show">OK</v-btn>
-          </v-date-picker>
-        </v-menu>
-        <AppointmentGraph :dates="dates2" />
+        <v-card elevation="0">
+          <v-card-title>Number of appointments</v-card-title>
+          <v-card-text>
+            <v-row>
+              <v-col cols="3">
+                <v-menu
+                  ref="menu"
+                  v-model="menu"
+                  :close-on-content-click="false"
+                  :return-value.sync="dates"
+                  transition="scale-transition"
+                  offset-y
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-model="dates"
+                      label="Date interval"
+                      prepend-icon="mdi-calendar"
+                      readonly
+                      v-bind="attrs"
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker v-model="dates" range no-title scrollable>
+                    <v-spacer></v-spacer>
+                    <v-btn text color="primary" @click="menu = false"
+                      >Cancel</v-btn
+                    >
+                    <v-btn text color="primary" @click="show">OK</v-btn>
+                  </v-date-picker>
+                </v-menu>
+              </v-col>
+            </v-row>
+            <AppointmentGraph :dates="dates2" />
+          </v-card-text>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
