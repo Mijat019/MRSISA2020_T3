101e5a03970aa2c2e5ded59311a89cb2ecf13b37
mijat.miletic98 <mijat.miletic98@gmail.com>
2020-06-10 03:00:48 +0200

    Implement show clinic on map feature

--- client/src/components/ClinicAdmin/BasicInfoTab/BasicInfo.vue
+++ client/src/components/ClinicAdmin/BasicInfoTab/BasicInfo.vue
@@ -1,96 +1,107 @@
 <template>
   <div>
     <v-card>
-        <v-card-title>Basic information</v-card-title>
-        <v-card-text>
-            <v-form v-model="formValid">
-                <v-list dense max-width="50%">
-                    <v-list-item>
-                        <v-list-item-icon>
-                            <v-icon>mdi-bank</v-icon>
-                        </v-list-item-icon>
-                        <v-list-item-content>
-                            <v-list-item-title>Clinic name</v-list-item-title>
-                            <v-text-field :rules="rules" v-model="myClinic.name"></v-text-field>
-                        </v-list-item-content>
-                    </v-list-item>
+      <v-card-title>Basic information</v-card-title>
+      <v-card-text>
+        <v-form v-model="formValid">
+          <v-list dense max-width="50%">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-bank</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>Clinic name</v-list-item-title>
+                <v-text-field
+                  :rules="rules"
+                  v-model="myClinic.name"
+                ></v-text-field>
+              </v-list-item-content>
+            </v-list-item>
 
-                    <v-list-item>
-                        <v-list-item-icon>
-                            <v-icon>mdi-dots-horizontal</v-icon>
-                        </v-list-item-icon>
-                        <v-list-item-content>
-                            <v-list-item-title>Description</v-list-item-title>
-                            <v-textarea :rules="rules" v-model="myClinic.description"></v-textarea>
-                        </v-list-item-content>
-                    </v-list-item>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-dots-horizontal</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>Description</v-list-item-title>
+                <v-textarea
+                  :rules="rules"
+                  v-model="myClinic.description"
+                ></v-textarea>
+              </v-list-item-content>
+            </v-list-item>
 
-                    <v-list-item>
-                        <v-list-item-icon>
-                            <v-icon>mdi-map-marker</v-icon>
-                        </v-list-item-icon>
-                        <v-list-item-content>
-                            <v-list-item-title>Address</v-list-item-title>
-                            <v-text-field :rules="rules" v-model="myClinic.address"></v-text-field>
-                        </v-list-item-content>
-                    </v-list-item>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-map-marker</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>Address</v-list-item-title>
+                <v-text-field
+                  :rules="rules"
+                  v-model="myClinic.address"
+                ></v-text-field>
+              </v-list-item-content>
+            </v-list-item>
 
-                    <v-list-item>
-                        <v-list-item-icon>
-                            <v-icon>mdi-map</v-icon>
-                        </v-list-item-icon>
-                        <v-list-item-content>
-                            <v-list-item-title>Map</v-list-item-title>
-                            <div style="height: 100px; background-color: grey;">[Staviti mapu ovde]</div>
-                        </v-list-item-content>
-                    </v-list-item>
-                </v-list>
-            </v-form>
-        </v-card-text>
+            <div id="map" style="width: 100%; height: 300px">
+              <ClinicLocation
+                :address="myClinic.address"
+                :city="myClinic.city"
+                :country="myClinic.country"
+              />
+            </div>
+          </v-list>
+        </v-form>
+      </v-card-text>
 
-        <v-card-actions v-if="formValid">
-            <v-btn color="primary" width="200" height="50" @click="saveClinic()">save</v-btn>
-        </v-card-actions>
+      <v-card-actions v-if="formValid">
+        <v-btn color="primary" width="200" height="50" @click="saveClinic()"
+          >save</v-btn
+        >
+      </v-card-actions>
     </v-card>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
+import ClinicLocation from './ClinicLocation.vue';
+import { mapGetters, mapActions } from 'vuex';
 
 export default {
-  name: "BasicInfo",
+  components: { ClinicLocation },
 
-    data() {
-        return {
-            formValid: false,
-            rules: [
-                v => !!v || 'Required'
-            ]
-        }
-    },
+  name: 'BasicInfo',
+
+  data() {
+    return {
+      formValid: false,
+      rules: [v => !!v || 'Required'],
+    };
+  },
+
   methods: {
-      ...mapActions({
-          getMyClinicAction: "clinicAdmins/getMyClinicAction",
-          updateClinicAction: "clinics/updateClinicAction"
-      }),
+    ...mapActions({
+      getMyClinicAction: 'clinicAdmins/getMyClinicAction',
+      updateClinicAction: 'clinics/updateClinicAction',
+    }),
 
-      saveClinic() {
-          if (this.formValid) {
-              this.updateClinicAction(this.myClinic);
-          }
+    async saveClinic() {
+      if (this.formValid) {
+        await this.updateClinicAction(this.myClinic);
       }
+    },
   },
 
   mounted() {
-      this.getMyClinicAction();
+    this.getMyClinicAction();
   },
 
   computed: {
-      ...mapGetters({
-          myClinic: "clinicAdmins/getMyClinic"
-      })
-  }
+    ...mapGetters({
+      myClinic: 'clinicAdmins/getMyClinic',
+    }),
+  },
 };
 </script>
 
